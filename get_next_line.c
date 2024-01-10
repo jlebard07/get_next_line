9fb2c1beaf33c69dfd880bc7937c80e605fe6b4b
Jean Lebard <jlebard@bess-f4r4s2.clusters.42paris.fr>
2024-01-10 13:18:52 +0100

    premier jet

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jlebard <jlebard@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/10 11:40:03 by jlebard           #+#    #+#             */
+/*   Updated: 2024/01/10 13:18:23 by jlebard          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	static char		*buffer;
+	size_t			count;
+	FILE			*file;
+
+	file = fd;
+	buffer = malloc(count + 1);
+	read(file, buffer, count);
+	//fonction pour identifier si dans les 
+	//count bytes lus il y a un \n 
+}
